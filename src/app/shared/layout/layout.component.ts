f971d3f426172d7d96cee2f037da14f2b1109352
tanasi111 <ivantanasi111@gmail.com>
2020-01-04 13:24:23 +0100

    layout

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LayoutService } from 'src/app/layout.service';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private layoutService: LayoutService
+  ) { }
 
   ngOnInit() {
+    this.initLayout();
+  }
+
+  initLayout() {
+    const layout: string = window.localStorage.getItem('layout');
+    if (layout !== 'GRID_LAYOUT' && layout !== 'LIST_LAYOUT') {
+      window.localStorage.setItem('layout', 'GRID_LAYOUT');
+      this.layoutService.gridLayout = true;
+    } else if (layout === 'GRID_LAYOUT') {
+      this.layoutService.gridLayout = true;
+    } else if (layout === 'LIST_LAYOUT') {
+      this.layoutService.gridLayout = false;
+    }
+  }
+
+  setLayout(layout: string) {
+    window.localStorage.setItem('layout', layout);
+    this.initLayout();
   }
 
 }
