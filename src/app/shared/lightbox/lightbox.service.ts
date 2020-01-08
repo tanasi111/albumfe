53e942911217caba6e505d29f016dbfa28b89be6
tanasi111 <ivantanasi111@gmail.com>
2020-01-08 17:12:57 +0100

    lightbox

--- src/app/shared/lightbox/lightbox.service.ts
+++ src/app/shared/lightbox/lightbox.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,8 @@ export class LightboxService {
   previousImageUrl: string;
   imageUrl: string;
   imageId: number;
+  firstImageId: number;
+  lastImageId: number;
   nextImageUrl: string;
 
   showLightbox: boolean;
@@ -20,6 +23,8 @@ export class LightboxService {
   openLightbox(imageUrl: string, imageId: number) {
     this.imageUrl = imageUrl;
     this.imageId = imageId;
+    this.firstImageId = this.images[0].id;
+    this.lastImageId = this.images[this.images.length - 1].id;
     this.showLightbox = true;
     const $body = window.document.body;
     $body.style.overflow = 'hidden';
@@ -32,8 +37,11 @@ export class LightboxService {
   }
 
   changeSlide(count: number) {
-    this.imageId = count + this.imageId;
-    this.imageUrl = `https://i.picsum.photos/id/${this.imageId}/640/480.jpg`;
+    const id = count + this.imageId;
+    if (id >= this.firstImageId && id <= this.lastImageId) {
+      this.imageId = id;
+      this.imageUrl = `https://i.picsum.photos/id/${this.imageId}/640/480.jpg`;
+    }
   }
 
 }
