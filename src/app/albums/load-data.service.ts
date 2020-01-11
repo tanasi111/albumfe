23ccb509ca371f634aedb21c9b072f13669cb681
tanasi111 <ivantanasi111@gmail.com>
2020-01-11 17:13:33 +0100

    data

--- src/app/albums/load-data.service.ts
+++ src/app/albums/load-data.service.ts
@@ -22,7 +22,7 @@ export class LoadDataService {
 
     if (this.firstPosition === 0 && this.fullAlbums.length >= 7) {
       this.albums = this.fullAlbums.slice(this.firstPosition, this.lastPosition);
-      this.firstPosition = this.lastPosition + 1;
+      this.firstPosition = this.lastPosition;
       this.lastPosition = this.firstPosition + 7;
     } else if (this.firstPosition === 0 && this.fullAlbums.length < 7) {
       this.albums = this.fullAlbums;
@@ -34,12 +34,14 @@ export class LoadDataService {
 
     if (this.fullAlbums.length >= this.lastPosition) {
       this.albums.push(...this.fullAlbums.slice(this.firstPosition, this.lastPosition));
-      this.firstPosition = this.lastPosition + 1;
+      this.firstPosition = this.lastPosition;
       this.lastPosition = this.firstPosition + 7;
-    } else {
+    } else if (this.fullAlbums.length > this.firstPosition) {
       this.albums.push(...this.fullAlbums.slice(this.firstPosition, this.fullAlbums.length));
+      this.firstPosition = this.fullAlbums.length;
+    } else {
+      console.log('no more data');
     }
 
-    console.log('load more albums');
   }
 }
